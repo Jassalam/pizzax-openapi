8997e1547ef1018e4db2fbf77356ed4dac843132
Jasveer <jasveerk1980@gmail.com>
2024-09-17 18:42:43 +0200

    first commit

---
+++ src/main/java/com/example/pizzax/PizzaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizzax/PizzaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
